c4e226fff3572154bea4956fb3e5b577abd079e1
Louis Duret-Robert <louisduret@gmail.com>
2025-08-30 20:08:29 +0200

    [Core] Base classes

---
+++ bad/__init__.py
@@ -0,0 +1,12 @@
+# ╔═══════════════════════════════════════════════════════════════════════════╗
+# ║ Project:     bad - Box-and-Arrow Diagram tool                             ║
+# ║ Author:      Louis Duret-Robert - louisduret@gmail.com                    ║
+# ║ Website:     louis-dr.github.io                                           ║
+# ║ License:     MIT License                                                  ║
+# ║ File:        __init__.py                                                  ║
+# ╚═══════════════════════════════════════════════════════════════════════════╝
+
+
+
+if __name__ == '__main__':
+  pass
---
+++ bad/bad.py
@@ -0,0 +1,89 @@
+# ╔═══════════════════════════════════════════════════════════════════════════╗
+# ║ Project:     bad - Box-and-Arrow Diagram tool                             ║
+# ║ Author:      Louis Duret-Robert - louisduret@gmail.com                    ║
+# ║ Website:     louis-dr.github.io                                           ║
+# ║ License:     MIT License                                                  ║
+# ║ File:        bad.py                                                       ║
+# ╚═══════════════════════════════════════════════════════════════════════════╝
+
+
+
+from dataclasses import dataclass
+
+
+
+@dataclass
+class Vector2:
+  x: float
+  y: float
+
+  def __add__(self, other):
+    return Vector2(self.x + other.x, self.y + other.y)
+
+  def __sub__(self, other):
+    return Vector2(self.x - other.x, self.y - other.y)
+
+  def __mul__(self, other):
+    if isinstance(other, Vector2):
+      return Vector2(self.x * other.x, self.y * other.y)
+    else:
+      return Vector2(self.x * other, self.y * other)
+
+  def __truediv__(self, other):
+    if isinstance(other, Vector2):
+      return Vector2(self.x / other.x, self.y / other.y)
+    else:
+      return Vector2(self.x / other, self.y / other)
+
+  def __repr__(self):
+    return f"[{self.x},{self.y}]"
+
+
+
+class Element:
+  """Base class for all schematic elements"""
+  def __init__(self, id=None, parent=None):
+    self.id     = id
+    self.parent = parent
+
+
+
+class LocatedElement(Element):
+  """Base class for schematic with a position"""
+  def __init__(self, id=None, parent=None, position=Vector2):
+    Element.__init__(self, id, parent)
+    self.position = position
+
+  @property
+  def x(self): return self.position.x
+
+  @property
+  def y(self): return self.position.y
+
+  def absolute_position(self) -> Vector2:
+    """Get absolute position in global coordinates"""
+    if self.parent: return self.position + self.parent.absolute_position()
+    else:           return self.position
+
+
+
+class BoundedElement(LocatedElement):
+  """Base class for schematic with a position and a size"""
+  def __init__(self, id=None, parent=None, position=Vector2, size=Vector2):
+    LocatedElement.__init__(self, id, parent, position)
+    self.size = size
+
+  @property
+  def width(self): return self.size.x
+
+  @property
+  def height(self): return self.size.y
+
+  @property
+  def x2(self): return self.x + self.width
+
+  @property
+  def y2(self): return self.y + self.height
+
+  @property
+  def center(self): return self.position + self.size / 2
